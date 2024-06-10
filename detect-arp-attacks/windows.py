17c9d8e083f3db02cdf012400b894f42dce37bb0
Laerson Hammes <laerson-hammes@gmail.com>
2024-06-10 18:13:09 -0300

    Change Get-NetAdapter command

--- detect-arp-attacks/windows.py
+++ detect-arp-attacks/windows.py
@@ -7,6 +7,6 @@ class Windows(System):
     @classmethod
     def get_interface(cls, /) -> str:
         return PowerShell.run(
-            'Get-NetAdapter -Physical | Where-Object Name -NE "Wi-Fi" | Select-Object Name | Format-List',
+            'Get-NetAdapter -Physical | Where-Object Status -EQ "Up" | Select-Object Name | Format-List',
             print_command=False
         ).strip().split(' : ')[1]
