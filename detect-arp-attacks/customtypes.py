54a9d3c8bceb23b5acf6e6f552c9a65658aef559
Laerson Hammes <laerson-hammes@gmail.com>
2024-06-10 11:28:32 -0300

    Revert "Detect ARP Attack"
    This reverts commit e3904a9bb524474548eb1d1c5445321b0396db9a.

--- detect-arp-attacks/customtypes.py
+++
@@ -1,6 +0,0 @@
-from typing import NewType
-
-
-IPv4DefaultGateway = NewType('IPv4DefaultGateway', str)
-IPv4Address = NewType('IPv4Address', str)
-MACAddr = NewType('MACAddr', str)
--- detect-arp-attacks/linux.py
+++
@@ -1,8 +0,0 @@
-from system import System
-
-
-class Linux(System):
-
-    @classmethod
-    def get_interface(cls, /) -> str:
-        ...
